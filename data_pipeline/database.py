ceb15d9630d690c8310ffc9028bbfd9741bfba3d
unknown <harika>
2026-09-13 10:26:32 +0530

    Link books to categories and prevent duplicates

--- data_pipeline/database.py
+++ data_pipeline/database.py
@@ -21,18 +21,27 @@ CREATE TABLE IF NOT EXISTS books (
     FOREIGN KEY (category_id) REFERENCES categories(category_id)
 )
 """)
+categories = ["Travel", "Mystery", "Science"]
 
+for category in categories:
+    cursor.execute(
+        "INSERT OR IGNORE INTO categories (category_name) VALUES (?)",
+        (category,)
+    )
 with open("data_pipeline/books.csv", "r", encoding="utf-8") as file:
     reader = csv.DictReader(file)
-
+    cursor.execute("DELETE FROM books")
     for book in reader:
+        category_id = ((reader.line_num - 2) % 3) + 1
+
         cursor.execute("""
-        INSERT INTO books (title, price_gbp, price_inr)
-        VALUES (?, ?, ?)
+        INSERT INTO books (title, price_gbp, price_inr, category_id)
+        VALUES (?, ?, ?, ?)
         """, (
-            book["title"],
-            book["price_gbp"],
-            book["price_inr"]
+        book["title"],
+        book["price_gbp"],
+        book["price_inr"],
+        category_id
         ))
 
 connection.commit()
